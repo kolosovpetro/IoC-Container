874bd5a967a07738b1ce1a44b52afb7d7444b1f8
petro kolosov <kolosovp94@gmail.com>
2020-10-05 00:22:06 +0300

    main updated

--- InversionOfControl.UI/Program.cs
+++ InversionOfControl.UI/Program.cs
@@ -1,4 +1,5 @@
-﻿using InversionOfControl.Entities.Implementations;
+﻿using System;
+using InversionOfControl.Entities.Implementations;
 using InversionOfControl.Entities.Interfaces;
 using InversionOfControl.Implementations;
 using InversionOfControl.Interfaces;
@@ -12,9 +13,14 @@ namespace InversionOfControl.UI
             IBuilder builder = new Builder();
             builder.AddSingleton<ILogger, Logger>();
             builder.AddSingleton<ILoggerService, LoggerService>();
+            builder.AddSingleton<IRandomNumber, RandomNumber>();
             var container = builder.Build();
             var loggerService = container.GetInstance<ILoggerService>();
             loggerService.Log("test");
+            var random1 = container.GetInstance<IRandomNumber>();
+            var random2 = container.GetInstance<IRandomNumber>();
+            Console.WriteLine($"Random number from singleton {nameof(random1)}: {random1.GetRandomNumber()}");
+            Console.WriteLine($"Random number from singleton {nameof(random2)}: {random2.GetRandomNumber()}");
         }
     }
 }
