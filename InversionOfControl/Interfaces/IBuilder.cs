602bb619388293f35afc5457c766848cf9188f61
petro kolosov <kolosovp94@gmail.com>
2020-10-04 14:04:25 +0300

    builder interface created

---
+++ InversionOfControl/Interfaces/IBuilder.cs
@@ -0,0 +1,30 @@
+﻿namespace InversionOfControl.Interfaces
+{
+    public interface IBuilder
+    {
+        /// <summary>
+        /// Adds a new transient item by implementation type to container.
+        /// </summary>
+        void AddTransient<TContract, TImplementation>();
+        
+        /// <summary>
+        /// Adds a new transient item by instance type to container.
+        /// </summary>
+        void AddTransient<TContract, TImplementation>(TImplementation instance);
+
+        /// <summary>
+        /// Adds a new singleton item by implementation type to container.
+        /// </summary>
+        void AddSingleton<TContract, TImplementation>();
+
+        /// <summary>
+        /// Adds a new singleton item by instance type to container.
+        /// </summary>
+        void AddSingleton<TContract, TImplementation>(TImplementation instance);
+
+        /// <summary>
+        /// Returns a service from dependencies list
+        /// </summary>
+        IContainer Build();
+    }
+}
